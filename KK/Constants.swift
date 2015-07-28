753206c95ae7e4156c0807123389d06d16b55284
lianghongbin <lianghongbin@vip.sohu.com>
2015-07-28 08:43:06 +0800

    添加常量文件

---
+++ KK/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  KK
+//
+//  Created by 宏彬 梁 on 7/28/15.
+//  Copyright (c) 2015 lianghongbin. All rights reserved.
+//
+
+import UIKit
+
+let insets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+let barTintColor = UIColor(red: 0.118, green: 0.125, blue: 0.133, alpha: 1.00)
+let titleDict: NSDictionary = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+
